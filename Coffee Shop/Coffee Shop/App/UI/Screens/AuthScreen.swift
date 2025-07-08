7b30a4658f9d44562633c5fced56bd953155e80f
seteamsh <seteamsh@gmail.com>
2025-07-08 19:48:16 +0500

    Configure

---
+++ Coffee Shop/Coffee Shop/App/UI/Screens/AuthScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AuthScreen.swift
+//  Coffee Shop
+//
+//  Created by Temirlan Zhumashov on 08.07.2025.
+//
+
+import SwiftUI
+
+struct AuthScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthScreen()
+}
