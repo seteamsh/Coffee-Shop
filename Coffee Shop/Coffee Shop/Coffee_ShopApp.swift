0474a8eadc16f57ed86e4a43e3ff3f13825efe3c
seteamsh <seteamsh@gmail.com>
2025-07-08 19:38:58 +0500

    initial

---
+++ Coffee Shop/Coffee Shop/Coffee_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_ShopApp.swift
+//  Coffee Shop
+//
+//  Created by Temirlan Zhumashov on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
