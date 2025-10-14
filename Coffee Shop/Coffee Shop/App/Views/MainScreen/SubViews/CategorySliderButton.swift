c83753fac497a057e3156cd567e8fedeeca6fc0a
seteamsh <seteamsh@gmail.com>
2025-10-15 00:01:09 +0500

    refactor category slider button

--- Coffee Shop/Coffee Shop/App/Views/MainScreen/SubViews/CategorySliderButton.swift
+++ Coffee Shop/Coffee Shop/App/Views/MainScreen/SubViews/CategorySliderButton.swift
@@ -9,18 +9,18 @@ struct CategorySliderButton: View {
         Button {
             action()
         } label: {
-            ZStack {
-                Rectangle()
-                    .fill(isActive ? Color.brownNormal : Color.categoryNotActive)
-                    .clipShape(RoundedRectangle(cornerRadius: 6))
-                    .frame(height: 29)
                 Text(name.name)
                     .fontSora(size: 14, weight: .regular, color: isActive ? Color.white : Color.grayNormal)
                     .lineLimit(1)
                     .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
-            }
-            
+                    .background(isActive ? Color.brownNormal : Color.categoryNotActive)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                    .frame(height: 29)
         }
         .padding(.trailing, isLast ? 0 : 16)
     }
+}
+
+#Preview {
+    CategorySliderButton(isActive: true, isLast: false, name: CategoryModel(id: 1, name: "fd", category: .americano), action: {})
 }
